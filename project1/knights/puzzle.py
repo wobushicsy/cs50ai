0a12a658e3ad43fa5fda0263c02aee97289d4420
wobushicsy <csy657703828@163.com>
2023-03-30 15:08:41 +0800

    Knights done

--- project1/knights/puzzle.py
+++ project1/knights/puzzle.py
@@ -11,22 +11,49 @@ CKnave = Symbol("C is a Knave")
 
 # Puzzle 0
 # A says "I am both a knight and a knave."
+Asay0 = And(AKnight, AKnave)
 knowledge0 = And(
     # TODO
+    Or(AKnave, AKnight), 
+    Implication(AKnight, Not(AKnave)),
+    Implication(AKnave, Not(AKnight)),
+    Implication(AKnight, Asay0), 
+    Implication(AKnave, Not(Asay0)), 
 )
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
+Asay1 = And(AKnave, BKnave)
 knowledge1 = And(
     # TODO
+    Or(AKnight, AKnave), 
+    Or(BKnight, BKnave),
+    Implication(AKnight, Not(AKnave)), 
+    Implication(AKnave, Not(AKnight)), 
+    Implication(BKnight, Not(BKnave)),
+    Implication(BKnave, Not(BKnight)),
+    Implication(AKnight, Asay1), 
+    Implication(AKnave, Not(Asay1))
 )
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
+Asay2 = Or(And(AKnight, BKnight), And(AKnave, BKnave))
+Bsay2 = Or(And(AKnight, BKnave), And(AKnave, BKnight))
 knowledge2 = And(
     # TODO
+    Or(AKnight, AKnave), 
+    Or(BKnight, BKnave), 
+    Implication(AKnight, Not(AKnave)), 
+    Implication(AKnave, Not(AKnight)), 
+    Implication(BKnight, Not(BKnave)),
+    Implication(BKnave, Not(BKnight)),
+    Implication(AKnight, Asay2), 
+    Implication(AKnave, Not(Asay2)), 
+    Implication(BKnight, Bsay2), 
+    Implication(BKnave, Not(Bsay2))
 )
 
 # Puzzle 3
@@ -34,8 +61,29 @@ knowledge2 = And(
 # B says "A said 'I am a knave'."
 # B says "C is a knave."
 # C says "A is a knight."
+Asayin31 = Symbol("I am a knight")
+Asayin32 = Symbol("I am a knave")
+Asay3 = Or(Asayin31, Asayin32)
+Bsay31 = Asayin32
+Bsay32 = CKnave
+Csay3 = AKnight
 knowledge3 = And(
     # TODO
+    Or(AKnight, AKnave), 
+    Or(BKnight, BKnave), 
+    Or(CKnight, CKnave),
+    Implication(AKnight, Not(AKnave)), 
+    Implication(AKnave, Not(AKnight)), 
+    Implication(BKnight, Not(BKnave)),
+    Implication(BKnave, Not(BKnight)),
+    Implication(CKnight, Not(CKnave)), 
+    Implication(CKnave, Not(CKnight)),
+    Implication(AKnight, Asay3), 
+    Implication(AKnave, Not(Asay3)), 
+    Implication(BKnight, And(Bsay31, Bsay32)),
+    Implication(BKnave, And(Not(Bsay31), Not(Bsay32))), 
+    Implication(CKnight, Csay3), 
+    Implication(CKnave, Not(Csay3))
 )
 
 
